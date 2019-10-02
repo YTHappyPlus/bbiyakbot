b8636a293e5d44c8dac3fa86677243e503e91d3a
HappyPlus <31878663+YTHappyPlus@users.noreply.github.com>
2019-10-02 14:43:21 +0900

    Update bbiyakbot.py

--- bbiyakbot.py
+++ bbiyakbot.py
@@ -3,13 +3,10 @@ import discord
 import datetime
 import os
 
+
 client = discord.Client()
 
-# 1-6에서 생성된 토큰을 이곳에 입력해주세요.
-access_token = os.environ["BOT_TOKEN"]
-token = "acces_token"
 
-# 봇이 구동되었을 때 동작되는 코드입니다.
 @client.event
 async def on_ready():
 
@@ -35,6 +32,7 @@ async def on_message(message):
         await message.channel.send("삐약-BBIYAK")
     if message.content.startswith('!커멘드'):
         await message.channel.send("삐약, 치킨맛있졍 ,BBI약팩, 나, BBIYAK 등이 있습니다. 꼭 ! 를 붙이세요!! ")
-        
-client.run(token)
+
+acces_token = os.environ["BOT_TOKEN"]     
+client.run(acces_toekn)
 
